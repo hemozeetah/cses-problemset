08a478eedddef9b77e70a4d382e22ae0e9589d7b
hemozeetah <ayhamdabah@gmail.com>
2025-11-05 13:16:42 +0300

    maximum subarray sum II problem using monoqueue

--- Sorting-and-Searching/Maximum_Subarray_Sum_II.cpp
+++ Sorting-and-Searching/Maximum_Subarray_Sum_II.cpp
@@ -6,6 +6,96 @@
 #define dout(...) void(0)
 #endif
 
+template <typename T, typename F>
+struct monostack {
+  F op;
+  std::vector<std::pair<T, T>> st;
+
+  monostack(const F &f) : op(f) {}
+
+  inline bool empty() const {
+    return st.empty();
+  }
+
+  inline size_t size() const {
+    return st.size();
+  }
+
+  inline void clear() {
+    st.clear();
+  }
+
+  inline void reserve(int n) {
+    st.reserve(n);
+  }
+
+  inline void push(T x) {
+    if (st.empty()) {
+      st.emplace_back(x, x);
+    } else {
+      st.emplace_back(x, op(st.back().second, x));
+    }
+  }
+
+  inline void pop() {
+    st.pop_back();
+  }
+
+  inline std::pair<T, T> top() const {
+    return st.back();
+  }
+};
+
+template <typename T, typename F>
+struct monoque {
+  F op;
+  monostack<T, F> p, s;
+
+  monoque(const F &f) : op(f), p(f), s(f) {}
+
+  inline bool empty() const {
+    return p.empty() && s.empty();
+  }
+
+  inline void clear() {
+    p.clear();
+    s.clear();
+  }
+
+  inline size_t size() const {
+    return p.size() + s.size();
+  }
+
+  inline void push(T x) {
+    s.push(x);
+  }
+
+  inline void pop() {
+    if (p.empty()) {
+      __prepare_front();
+    }
+    p.pop();
+  }
+
+  inline std::pair<T, T> front() {
+    if (p.empty()) {
+      __prepare_front();
+    }
+    if (s.empty()) {
+      return p.top();
+    }
+    return std::pair(p.top().first, op(p.top().second, s.top().second));
+  }
+
+  inline void __prepare_front() {
+    p.reserve(s.size());
+    while (!s.empty()) {
+      p.push(s.top().first);
+      s.pop();
+    }
+  }
+};
+
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int n, a, b;
@@ -19,13 +109,16 @@ int main() {
     p[i + 1] = p[i] + x[i];
   }
   int64_t ans = INT64_MIN;
-  std::multiset<int64_t> ms;
+  auto op = [](const int64_t &x, const int64_t &y) {
+    return std::min(x, y);
+  };
+  monoque<int64_t, decltype(op)> mq(op);
   for (int i = a - 1; i < n; i++) {
     if (i >= b) {
-      ms.erase(ms.find(p[i - b]));
+      mq.pop();
     }
-    ms.insert(p[i - a + 1]);
-    ans = std::max(ans, p[i + 1] - *ms.begin());
+    mq.push(p[i - a + 1]);
+    ans = std::max(ans, p[i + 1] - mq.front().second);
   }
   std::cout << ans << '\n';
   return 0;
